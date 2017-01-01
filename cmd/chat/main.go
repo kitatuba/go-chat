c6824cbbb5e00ae71bbc58e9054ca1b6c20d5f42
kitatuba <hokudenyoku@gmail.com>
2017-01-01 17:16:27 +0900

    1.1.1.2

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-			<head>
-				<title>チャット</title>
-			</head>
-			<body>
-				チャットしましょう!
-			</body>
-		</html>
-	`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// http.Handler型に適合させるため、ServeHTTPメソッドを実装
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("./../../templates", t.filename)))
 	})
+
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// WEBサーバの開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndserve:", err)
 	}
