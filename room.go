64eadead8c99d95180ae0c386b72f5e8553d2730
kitatuba <hokudenyoku@gmail.com>
2017-01-03 18:41:48 +0900

    1.2.4

--- room.go
+++ room.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// プロジェクト内で何度もハードコートされる可能性がある値については、定数として宣言
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+// upgrader HTTP接続をアップグレードする
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+// ServeHTTP http.Handler型に適合 HTTPハンドラとして扱えるようになる
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	// WebSocketコネクションを取得
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+
+	// チャットクライアント(ユーザ)を生成
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	// 該当のチャットルームのjoinチャネルにクライアントを送信
+	r.join <- client
+
+	// クライアントの終了時に退室処理を必ず行う
+	defer func() { r.leave <- client }()
+
+	// 別goroutineに担当させる
+	go client.write()
+
+	// sendチャネルのクローズまで接続を保持
+	client.read()
+}
+
 type room struct {
 	// forward 他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
